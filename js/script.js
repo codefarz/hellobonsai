4391fd11c922ad40a847d172af909c7f8f53ccc7
codefarz <codefarz@gmail.com>
2023-01-24 14:39:20 -0500

    estructure & order js file

--- js/script.js
+++ js/script.js
@@ -1,17 +1,65 @@
+// mobile menu
+
 const iconMenu = document.querySelector(".icon-menu");
 const menuMobile = document.querySelector("#menu-mobile");
+
 const linkMenuMobileOne = document.querySelector(".link-menu-mobile-one");
-const arrowMenuMobileOne = document.querySelector(".arrow-down-one");
 const linkMenuMobileTwo = document.querySelector(".link-menu-mobile-two");
+const arrowMenuMobileOne = document.querySelector(".arrow-down-one");
 const arrowMenuMobileTwo = document.querySelector(".arrow-down-two");
+const productListMobile = document.querySelector(".product-list-mobile");
+const templatesList = document.querySelector("#templates-list");
+const productList = document.querySelector("#product-list");
+
+iconMenu.addEventListener('click', () => {
+    menuMobile.classList.toggle("inactive");
+});
+
+linkMenuMobileOne.addEventListener('click', () => {
+    arrowMenuMobileOne.classList.toggle("arrow-transform");
+    productList.classList.toggle("inactive");
+});
+
+linkMenuMobileTwo.addEventListener('click', () => {
+    arrowMenuMobileTwo.classList.toggle("arrow-transform");
+    templatesList.classList.toggle("inactive");
+});
+
+// -------------------------------------------
+
+// pricing cards
+
 const btnMonthly = document.querySelector("#monthly");
 const btnYearly = document.querySelector("#yearly");
 const priceStarter = document.querySelector("#price-starter");
 const priceProfessional = document.querySelector("#price-professional");
 const priceBusiness = document.querySelector("#price-business");
-const productListMobile = document.querySelector(".product-list-mobile");
-const templatesList = document.querySelector("#templates-list");
-const productList = document.querySelector("#product-list");
+
+btnMonthly.addEventListener('click', toggleBtn);
+btnYearly.addEventListener('click', toggleBtn);
+
+function toggleBtn() {
+    btnMonthly.classList.toggle("toggle-button-pricing");
+    btnYearly.classList.toggle("toggle-button-pricing");
+    changePricing();
+}
+
+function changePricing() {
+    if (priceStarter.textContent == 17) {
+        priceStarter.innerText = 24;
+        priceProfessional.innerText = 39;
+        priceBusiness.innerText = 79;
+    } else {
+        priceStarter.innerText = 17;
+        priceProfessional.innerText = 32;
+        priceBusiness.innerText = 52;
+    }
+}
+
+// -------------------------------------------
+
+// nav menu
+
 const productDownMenu = document.querySelector("#product-down-menu");
 const industriesDownMenu = document.querySelector("#industries-down-menu");
 const templatesDownMenu = document.querySelector("#templates-down-menu");
@@ -52,19 +100,6 @@ const featuredFormTemplates = document.getElementsByClassName(
 );
 const pricingCards = document.querySelector('.pricing-cards')
 
-iconMenu.addEventListener("click", () => {
-    menuMobile.classList.toggle("inactive");
-});
-
-linkMenuMobileOne.addEventListener("click", () => {
-    arrowMenuMobileOne.classList.toggle("arrow-transform");
-    productList.classList.toggle("inactive");
-});
-
-linkMenuMobileTwo.addEventListener("click", () => {
-    arrowMenuMobileTwo.classList.toggle("arrow-transform");
-    templatesList.classList.toggle("inactive");
-});
 
 productTitleNav.onmouseover = () => {
     productDownMenu.classList.remove("inactive");
@@ -217,23 +252,5 @@ formTemplatesLink.addEventListener("click", () => {
     }
 });
 
-btnMonthly.addEventListener("click", toggleBtn);
-btnYearly.addEventListener("click", toggleBtn);
+// -------------------------------------------
 
-function toggleBtn() {
-    btnMonthly.classList.toggle("toggle-button-pricing");
-    btnYearly.classList.toggle("toggle-button-pricing");
-    changePricing();
-}
-
-function changePricing() {
-    if (priceStarter.textContent == 17) {
-        priceStarter.innerText = 24;
-        priceProfessional.innerText = 39;
-        priceBusiness.innerText = 79;
-    } else {
-        priceStarter.innerText = 17;
-        priceProfessional.innerText = 32;
-        priceBusiness.innerText = 52;
-    }
-}
